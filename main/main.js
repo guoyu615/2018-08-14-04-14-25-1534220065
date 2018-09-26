8f3e4ae38d972374cb64ce01a22a2231bb85a728
guoyu615 <guoyu0615@hotmail.com>
2018-09-26 23:55:59 +0800

    add my answer

--- main/main.js
+++ main/main.js
@@ -1,4 +1,28 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(n) {
+  var array = n.split("");
+  var lcdcode ={
+	  first:   ["._.","...","._.","._.","...","._.","._.","._.","._.","._."],
+      second:  ["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"],
+      third:   ["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"]
+    }
+  
+  var one ="";
+  var two ="";
+  var three ="";
+  
+  for (var i=0; i<array.length; i++){
+	  if (i == array.length-1){
+		one += lcdcode.first[array[i]]+"\n";
+	    two += lcdcode.second[array[i]]+"\n";
+	    three += lcdcode.third[array[i]]+"\n";  
+	  }
+	  else{
+	    one += lcdcode.first[array[i]]+" ";
+	    two += lcdcode.second[array[i]]+" ";
+	    three += lcdcode.third[array[i]]+" ";
+      }
+  }
+  var sum = one + two + three;
+  return sum;
+  
 };
